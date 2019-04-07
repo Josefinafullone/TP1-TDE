1309bf03d3c1765966b0753d9a45c54a6f8d3bf1
Josefinafullone <31524108+Josefinafullone@users.noreply.github.com>
2019-04-07 20:41:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@ int main(int argc, char* argv[]) {
         std::cerr << "Wrong input." << std::endl;
         return 1;
     }
-    //Create players with preferences
-    tournament my_tournament(strtol(argv[1], nullptr, 10), //argv[1] as long int (TODO: todavia no fue usado)
-                        std::string(argv[2]));         //argv[2] as string
-    //Split players by ranking
-    //Match players
+    
+    int preference=atoi(argv[1]);
+    if(preference-argc == 0 || argc==0)
+    {   
+        //Create players with preferences
+         tournament my_tournament(strtol(argv[1], nullptr, 10), //argv[1] as long int (TODO: todavia no fue usado)
+                               std::string(argv[2]));         //argv[2] as string
+         //Split players by ranking
+         //Match players
+    } else std::cerr << "Wrong input. The number of prefence must be a positive number" << std::endl; 
 
     return 0;
 }
